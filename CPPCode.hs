11f8b5e17280a13d52d57ec34192d43962b97d2e
Dillon Huff <dillonhuff@gmail.com>
2015-09-23 13:14:36 -0700

    More code for main and argument lists now in place

--- CPPCode.hs
+++ CPPCode.hs
@@ -1,7 +1,12 @@
 module CPPCode(CPPTopLevelItem,
                include, namespace, enum, function,
                CPPType,
-               void, int, char, ptr, objectType,
+               void, int, char, ptr, ref, objectType, constq,
+               templateObjectType, functionType,
+               CPPStmt,
+               returnStmt, exprStmt,
+               CPPExpr,
+               cppVar, functionCall,
                prettyCPP) where
 
 import Data.List as L
@@ -10,7 +15,7 @@ data CPPTopLevelItem
   = Include String
   | Namespace String
   | Enum String [String]
-  | Function CPPType String [(CPPType, String)]
+  | Function CPPType String [(CPPType, String)] [CPPStmt]
     deriving (Eq, Ord)
 
 include str = Include $ "\"" ++ str ++ "\""
@@ -21,14 +26,17 @@ function = Function
 instance Show CPPTopLevelItem where
   show (Include str) = "#include " ++ str
   show (Namespace str) = "using namespace " ++ str ++ ";"
-  show f@(Function tp name formalParams) = showFunction f
+  show f@(Function _ _ _ _) = showFunction f
   show (Enum enumName values) =
     "enum " ++ enumName ++ " {" ++
     (L.concat $ L.intersperse ", " values)
     ++ "};"
 
-showFunction (Function tp name formalParams) =
-  show tp ++ " " ++ name ++ showParamList formalParams ++ "{}"
+showFunction (Function tp name formalParams body) =
+  show tp ++ " " ++ name ++ showParamList formalParams ++ showBody body
+
+showBody stmts =
+  "{\n" ++ (L.concatMap (\stmt -> show stmt ++ "\n") stmts) ++ "\n}"
 
 showParamList ps =
   "(" ++ (L.concat $ L.intersperse ", " $ L.map showParam ps) ++ ")"
@@ -40,21 +48,74 @@ data CPPType
   | Int
   | Char
   | Ptr CPPType
+  | Ref CPPType
+  | QualifiedType Qualifier CPPType
   | ObjectType String
+  | FunctionType String
+  | TemplateObjectType String [CPPType]
     deriving (Eq, Ord)
 
 void = Void
 int = Int
 char = Char
 ptr = Ptr
+ref = Ref
+const = Const
+constq = QualifiedType Const
 objectType = ObjectType
+functionType = FunctionType
+templateObjectType = TemplateObjectType
 
 instance Show CPPType where
   show Void = "void"
   show Int = "int"
   show Char = "char"
   show (Ptr t) = show t ++ "*"
+  show (Ref t) = show t ++ "&"
   show (ObjectType n) = n
+  show (FunctionType n) = n
+  show (TemplateObjectType n ts) = n ++ showTemplateParamList ts
+  show (QualifiedType q t) = show q ++ " " ++ show t
+
+showTemplateParamList tps =
+  "<" ++ (L.concat $ L.intersperse ", " $ L.map show tps) ++ ">"
+
+data Qualifier
+  = Const
+    deriving (Eq, Ord)
+
+instance Show Qualifier where
+  show Const = "const"
+
+data CPPStmt
+  = ReturnStmt CPPExpr
+  | ExprStmt CPPExpr
+    deriving (Eq, Ord)
+
+returnStmt = ReturnStmt
+exprStmt = ExprStmt
+
+instance Show CPPStmt where
+  show (ReturnStmt expr) = "return " ++ show expr ++ ";"
+  show (ExprStmt expr) = show expr ++ ";"
+
+data CPPExpr
+  = FunctionCall String [CPPType] [CPPExpr]
+  | CPPVar String
+    deriving (Eq, Ord)
+
+instance Show CPPExpr where
+  show (CPPVar n) = n
+  show (FunctionCall str [] args) =
+    str ++ showArgList args
+  show (FunctionCall str tps args) =
+    str ++ showTemplateParamList tps ++ showArgList args
+
+showArgList args =
+  "(" ++ (L.concat $ L.intersperse ", " $ L.map show args) ++ ")"
+
+functionCall = FunctionCall
+cppVar = CPPVar
 
 prettyCPP :: [CPPTopLevelItem] -> String
 prettyCPP items = L.concat $ L.intersperse "\n" $ L.map show items
--- Syntax.hs
+++ Syntax.hs
@@ -77,18 +77,33 @@ fieldIDs ts = enum "FieldIDs" ids
     ids = L.nub $ L.concatMap (\rr -> fieldNames $ lrFieldSpace $ rrRegion rr) rrs
 
 topLevelTask ts =
-  function void "top_level_task" taskArgs
+  function void "top_level_task" taskArgs []
 
 taskFunctions ts = L.map taskFunction uniqueTasks
   where
     uniqueTasks = L.nub ts
 
 taskFunction t =
-  function void (taskName t) taskArgs
+  function void (taskName t) taskArgs []
 
 mainFunction ts =
-  function int "main" [(int, "argc"), (ptr $ ptr $ char, "argv")]
+  function int "main" [(int, "argc"), (ptr $ ptr $ char, "argv")] (mainBody ts)
+
+mainBody ts =
+  [exprStmt $ functionCall "HighLevelRuntime::set_top_level_task_id" [] [cppVar "TOP_LEVEL_TASK_ID"],
+   exprStmt $ functionCall "HighLevelRuntime::register_legion_task" [functionType "top_level_task"] [cppVar "TOP_LEVEL_TASK_ID", cppVar "Processor::LOC_PROC", cppVar "true", cppVar "false"]] ++
+  taskRegistrationCode ts ++
+  [returnStmt $ functionCall "HighLevelRuntime::start" [] [cppVar "argc", cppVar "argv"]]
+
+taskRegistrationCode ts = L.map registerTaskCode uniqueTasks
+  where
+    uniqueTasks = L.nub ts
+
+registerTaskCode t =
+  exprStmt $ functionCall "HighLevelRuntime::register_legion_task" [functionType $ taskName t] [cppVar $ taskName t, cppVar "Processor::LOC_PROC", cppVar "true", cppVar "false"]
 
 taskArgs =
-  [(objectType "Context", "ctx"),
+  [(constq $ ptr $ objectType "Task", "task"),
+   (constq $ ref $ templateObjectType "std::vector" [objectType "PhysicalRegion"], "regions"),
+   (objectType "Context", "ctx"),
    (ptr $ objectType "HighLevelRuntime", "runtime")]
