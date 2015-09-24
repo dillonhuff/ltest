26dea1eb14cd14483dc1efd31c8f99f8a15940f1
Dillon Huff <dillonhuff@gmail.com>
2015-09-24 16:46:24 -0700

    Can now run multiple tests and print the results

--- Main.hs
+++ Main.hs
@@ -10,9 +10,12 @@ import TestElaboration
 
 main :: IO ()
 main = do
-  execTestCase "first" testTasks
+  sequence_ $ L.map (\(testName, tasks) -> execTestCase testName tasks) tests
+  sequence_ $ L.map (\(testName, _) -> showTestResult testName) tests
+
+  {-execTestCase "first" testTasks
   depLines <- parseTestResult "first"
-  putStrLn $ depLines
+  putStrLn $ depLines-}
 
 legionSpyPath = "/Users/dillon/CppWorkspace/Legion/legion/tools/legion_spy.py"
 testPath = "/Users/dillon/Haskell/legion/ltest/cases/"
@@ -34,6 +37,11 @@ execTestCase testName tasks = do
 legionMakeFile name =
   "ifndef LG_RT_DIR\n$(error LG_RT_DIR variable is not defined, aborting build)\nendif\nDEBUG=1\nOUTPUT_LEVEL=LEVEL_DEBUG\nSHARED_LOWLEVEL=1\nCC_FLAGS=-DLEGION_SPY\nOUTFILE\t:= " ++ name ++ "\nGEN_SRC\t:= " ++ name ++ ".cc" ++ "\ninclude $(LG_RT_DIR)/runtime.mk"
 
+showTestResult :: String -> IO ()
+showTestResult testName = do
+  res <- parseTestResult testName
+  putStrLn res
+
 parseTestResult :: String -> IO String
 parseTestResult testName = do
   legionSpyOutput <- readFile $ testPath ++ "/" ++ testName ++ "/result.txt"
@@ -43,8 +51,14 @@ parseLegionSpyLog :: String -> String
 parseLegionSpyLog logStr =
   let logLines = lines logStr
       lineSuffixes = map (dropWhile isSpace) logLines in
-   head $ filter (isPrefixOf "Mapping Dependence Errors:") lineSuffixes
+   head $ filter (isInfixOf "Mapping Dependence Errors:") lineSuffixes
 
-testTasks =
+tests = [("test1", testTasks1), ("test2", testTasks2)]
+
+testTasks1 =
   [task "task_A" $ [regionRequirement (logicalRegion "lrA" (indexSpace "i1" 0 1) (fieldSpace "fsA" ["X", "Y"])) RW EXCLUSIVE],
    task "task_B" $ [regionRequirement (logicalRegion "lrB" (indexSpace "i2" 0 123) (fieldSpace "fsB" ["K", "Z"])) RO EXCLUSIVE]]
+
+testTasks2 =
+  [task "task_A" $ [regionRequirement (logicalRegion "lrA" (indexSpace "i1" 0 1) (fieldSpace "fsA" ["X", "Y"])) RW EXCLUSIVE],
+   task "task_B" $ [regionRequirement (logicalRegion "lrA" (indexSpace "i1" 0 1) (fieldSpace "fsA" ["X", "Y"])) RW EXCLUSIVE]]
