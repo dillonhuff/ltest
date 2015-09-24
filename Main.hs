eab7d33cfb245da8a714898aa0fa6785d4be7198
Dillon Huff <dillonhuff@gmail.com>
2015-09-24 15:39:45 -0700

    The system can now run a basic automated test and output the number of dependence analysis errors output by LegionSpy

--- Main.hs
+++ Main.hs
@@ -1,16 +1,21 @@
 module Main(main) where
 
+import Data.Char
+import Data.List as L
+
 import CPPCode
 import Syntax
 import SystemUtils
 import TestElaboration
 
 main :: IO ()
-main = execTestCase "first" testTasks
+main = do
+  execTestCase "first" testTasks
+  depLines <- parseTestResult "first"
+  putStrLn $ depLines
 
 legionSpyPath = "/Users/dillon/CppWorkspace/Legion/legion/tools/legion_spy.py"
 testPath = "/Users/dillon/Haskell/legion/ltest/cases/"
-testFileName = "dummy.cc"
 
 execTestCase :: String -> [Task] -> IO ()
 execTestCase testName tasks = do
@@ -19,13 +24,26 @@ execTestCase testName tasks = do
   writeFile (testDir ++ "/Makefile") (legionMakeFile testName)
   runCommandStrict $ "make -C " ++ testDir
   runCommandStrict $ testDir ++ "/" ++ testName ++ " " ++ spyFlags
-  runCommandStrict $ legionSpyPath ++ " -l spy.log"
+  runCommandStrict $ legionSpyPath ++ " -l " ++ spyFile ++ " > " ++ testResFile
   where
     testDir = testPath ++ testName
-    spyFlags = "-level 2 -cat legion_spy -logfile spy.log"
-
+    spyFlags = "-level 2 -cat legion_spy -logfile " ++ spyFile
+    spyFile = testDir ++ "/spy.log"
+    testResFile = testDir ++ "/result.txt"
+  
 legionMakeFile name =
   "ifndef LG_RT_DIR\n$(error LG_RT_DIR variable is not defined, aborting build)\nendif\nDEBUG=1\nOUTPUT_LEVEL=LEVEL_DEBUG\nSHARED_LOWLEVEL=1\nCC_FLAGS=-DLEGION_SPY\nOUTFILE\t:= " ++ name ++ "\nGEN_SRC\t:= " ++ name ++ ".cc" ++ "\ninclude $(LG_RT_DIR)/runtime.mk"
+
+parseTestResult :: String -> IO String
+parseTestResult testName = do
+  legionSpyOutput <- readFile $ testPath ++ "/" ++ testName ++ "/result.txt"
+  return $ parseLegionSpyLog legionSpyOutput
+
+parseLegionSpyLog :: String -> String
+parseLegionSpyLog logStr =
+  let logLines = lines logStr
+      lineSuffixes = map (dropWhile isSpace) logLines in
+   head $ filter (isPrefixOf "Mapping Dependence Errors:") lineSuffixes
 
 testTasks =
   [task "task_A" $ [regionRequirement (logicalRegion "lrA" (indexSpace "i1" 0 1) (fieldSpace "fsA" ["X", "Y"])) RW EXCLUSIVE],
